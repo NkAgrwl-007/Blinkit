b837d13425e5c6ca33a4cbfb21466415a25228d0
KSHITIZJAIN00 <114683009+KSHITIZJAIN00@users.noreply.github.com>
2025-01-14 19:06:24 +0530

    xyz

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,21 +4,19 @@ import User from "../models/User.js";
 
 const router = express.Router();
 
-// Signup route
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Check if the user already exists
+  
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash the password
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
 
@@ -28,18 +26,16 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// Login route
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Find the user by email
+
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Compare the password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
